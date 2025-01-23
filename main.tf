c921852f2fb4197c16b93ed691f51219b4140e94
plzdontbanme <158291695+plzdontbanme@users.noreply.github.com>
2025-01-23 11:55:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "azuread_client_config" "current" {}
 
 module "assertion" {
   source  = "plzdontbanme/assertion/null"
-  version = "0.1.4"
+  version = "0.0.0"
   # As an example, we check that the tenant ID is correct
   condition     = data.azuread_client_config.current.tenant_id == var.tenant_id
   error_message = "This configuration is being applied to the wrong tenant!!"
